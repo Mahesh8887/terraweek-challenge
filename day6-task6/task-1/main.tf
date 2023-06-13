dd3b00c1d31b69752eff4b427fd0b75a49ae8ad5
Mahesh Biradar <119730245+Mahesh8887@users.noreply.github.com>
2023-06-13 17:29:26 +0530

    Update main.tf

--- day6-task6/task-1/main.tf
+++ day6-task6/task-1/main.tf
@@ -1,1 +1,20 @@
+### $ vim provider.tf
 
+ # Authentication using access key
+ provider "aws" {
+     access_key = "AKIAZDXMNSUCC64NNYJQ"
+     secret_key = "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"
+     region = "ap-southeast-1"
+ }
+
+#### $ vim resource.tf
+
+resource "aws_instance" "practice" {
+     ami = "ami-0df7a207adb9748c7"
+     instance_type = "t2.micro" # free tier
+     security_groups = ["default"]
+     key_name = "project-1"
+     tags = {
+         Name = "PracticeInstance"
+     }
+ }
